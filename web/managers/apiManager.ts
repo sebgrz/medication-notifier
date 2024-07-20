4e3454b4b0a24e89929685ea32442bba70f62fb3
Seba G <sebag.git@gmail.com>
2024-07-20 10:27:56 +0200

    feat(web) cookies manager

--- web/managers/apiManager.ts
+++ web/managers/apiManager.ts
@@ -1,21 +1,35 @@
 import CookieManager from "./cookieManager";
 
+enum Header {
+  CLIENT_ID = "X-Client-Id",
+  USER_AGENT = "User-Agent"
+}
+
 class ApiManager {
   private static BASE_URL = "http://localhost:8080"
 
   static authRegister = async (username: string, password: string): Promise<boolean> => {
     const body = JSON.stringify({ username: username, password: password });
-    const clientId = crypto.randomUUID();
+    const headers = ApiManager.getRequiredHeaders();
 
     try {
       await fetch(ApiManager.BASE_URL + "/api/auth/register",
-        { method: "POST", body: body, headers: { 'X-Client-Id': clientId, "User-Agent": window.navigator.userAgent } }
+        { method: "POST", body: body, headers: headers }
       );
     } catch {
       return false;
     }
-    CookieManager.setCookie("client-id", clientId, 5)
+    CookieManager.set("client-id", headers[Header.CLIENT_ID], 5 * 365)
     return true;
+  }
+
+  private static getRequiredHeaders = (): { [key: string]: string; } => {
+    const clientId = CookieManager.get("client-id") ?? crypto.randomUUID();
+    Headers
+    return {
+      [Header.CLIENT_ID]: clientId,
+      [Header.USER_AGENT]: window.navigator.userAgent
+    };
   }
 }
 
--- web/managers/cookieManager.ts
+++ web/managers/cookieManager.ts
@@ -1,6 +1,6 @@
 class CookieManager {
-  static setCookie = (cookieName: string, cookieValue: string, ageYears: number): string | undefined => {
-    const year = 60 * 60 * 24 * 365 * ageYears;
+  static set = (cookieName: string, cookieValue: string, ageInDays: number): string | undefined => {
+    const year = 60 * 60 * 24 * ageInDays;
     document.cookie = `${cookieName}=${cookieValue}; max-age=${year}; path=/; SameSite=Lax; Secure;`;
 
     const cookie = document.cookie
@@ -9,6 +9,26 @@ class CookieManager {
       ?.split("=")[1];
     return cookie;
   }
+
+  static get = (cookieName: string): string | undefined => {
+    const cookies = document.cookie?.split(';');
+    if (!cookies) {
+      return undefined;
+    }
+
+    for (let cookie in cookies) {
+      const parts = cookie.split('=');
+
+      if (parts[0] === cookieName) {
+        return parts[1];
+      }
+    }
+
+    return undefined;
+  }
+
+  static delete = (cookieName: string) =>
+    document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
 }
 
 export default CookieManager;
