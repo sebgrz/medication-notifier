b1577de751d6bfdc1a377a7855643dbf171ba3cf
Seba G <sebag.git@gmail.com>
2024-08-28 22:17:32 +0200

    feat(backend) fetch newest activities from activity_log

--- backend/data/activity_log.go
+++ backend/data/activity_log.go
@@ -1,5 +1,13 @@
 package data
 
+type ActivityLog struct {
+	ClientId         string
+	UserId           string
+	RefreshTokenHash string
+	ExpireTime       int64
+}
+
 type ActivityLogDataService interface {
-	Add(clientId, userId, refreshTokenHash string, expiryTime int64) error
+	Add(clientId, userId, refreshTokenHash string, expireTime int64) error
+	FetchAllNewestGroupedByClientId() []ActivityLog
 }
--- backend/data/db/activity_log_db.go
+++ backend/data/db/activity_log_db.go
@@ -3,6 +3,9 @@ package db
 import (
 	"context"
 
+	"medication-notifier/data"
+	"medication-notifier/utils/logger"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -27,4 +30,54 @@ func (s *DbActivityLogDataService) Add(clientId, userId, refreshTokenHash string
 		expiryTime,
 	)
 	return err
+}
+
+func (s *DbActivityLogDataService) FetchAllNewestGroupedByClientId() []data.ActivityLog {
+	sql := `
+	select
+		client_id,
+		user_id,
+		refresh_token_hash,
+		expire_time
+	from
+	(
+		select
+			client_id,
+			user_id,
+			refresh_token_hash,
+			expire_time,
+			row_number() over
+				(partition by user_id order by expire_time desc)
+				as row_number
+		from
+			med.login_activity_log
+	) data
+	where
+		data.row_number = 1
+	`
+	result := []data.ActivityLog{}
+	rows, err := s.conn.Query(context.Background(), sql)
+	if err != nil {
+		logger.Error("fetch activity_log failed, err: %s", err)
+		return result
+	}
+
+	for rows.Next() {
+		var clientId string
+		var userId string
+		var refreshTokenHash string
+		var expireTime int64
+		if err := rows.Scan(&clientId, &userId, &refreshTokenHash, &expireTime); err != nil {
+			logger.Error("fetch activity_log scan failed, err: %s", err)
+			return []data.ActivityLog{}
+		}
+		result = append(result, data.ActivityLog{
+			ClientId:         clientId,
+			UserId:           userId,
+			RefreshTokenHash: refreshTokenHash,
+			ExpireTime:       expireTime,
+		})
+	}
+
+	return result
 }
